097921aa21f9aa0e8ecb49b8cf2e78881a034f16
Mohamed Elgohary <mohamed.abdelazim.elgohary@gmail.com>
2024-08-29 21:27:21 +0300

    init

---
+++ bin/student_management_system.dart
@@ -0,0 +1,5 @@
+import 'package:student_management_system/student_management_system.dart' as student_management_system;
+
+void main(List<String> arguments) {
+  print('Hello world: ${student_management_system.calculate()}!');
+}
